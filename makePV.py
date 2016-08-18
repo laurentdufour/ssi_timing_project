88b72f43d7b173862cae56a4bedd8ee48519311b
Jacco de Vries <jdevries@nikhef.nl>
2016-08-17 17:23:28 -0700

    updated PV with units

--- makePV.py
+++ makePV.py
@@ -1,12 +1,20 @@
 
 import numpy as np
+from units import mm, mm_to_ps, ps
 
-def makePV(meanx=0., meany=0., meanz=0., meanct=0., spreadx=0., spready=0., spreadz=50., spreadct=50., zctcorr = 0.) :
-  x = 0; y = 0; z = 0; ct = 0;
-  if not (meanx==0  and spreadx==0)  : x = np.random.normal(meanx, spreadx)
-  if not (meany==0  and spready==0)  : y = np.random.normal(meany, spready)
-  if not (meanz==0  and spreadz==0)  : z = np.random.normal(meanz, spreadz)
-  if not (meanct==0 and spreadct==0) : ct = np.random.normal(meanct + zctcorr * z, spreadct)
+def makePV(meanx=0.*mm, meany=0.*mm, meanz=0.*mm, meant=0.*mm_to_ps, 
+    spreadx=0.*mm, spready=0.*mm, spreadz=50.*mm, spreadt=50.*mm*mm_to_ps, 
+    ztcorr = 0.) :
 
-  return [ct,x,y,z]
+  '''
+  creates a PV (= list of [time (ns), x(mm), y(mm), z(mm)] )
+  '''
+
+  x = 0; y = 0; z = 0; t = 0;
+  if not (meanx==0 and spreadx==0) : x = np.random.normal(meanx, spreadx)
+  if not (meany==0 and spready==0) : y = np.random.normal(meany, spready)
+  if not (meanz==0 and spreadz==0) : z = np.random.normal(meanz, spreadz)
+  if not (meant==0 and spreadt==0) : t = np.random.normal(meant + ztcorr * z, spreadt)
+
+  return [t,x,y,z]
 
--- units.py
+++ units.py
@@ -7,3 +7,6 @@ cm = 10 * mm
 meter = 1000 * mm;
 
 ps = 1.0
+
+
+mm_to_ps = 3.3356 * ps
