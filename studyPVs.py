53f18e372e54a3fca1018f846e1e78bf913dc10c
Jacco de Vries <jdevries@nikhef.nl>
2016-08-17 17:34:53 -0700

    updated studyPVs

--- studyPVs.py
+++ studyPVs.py
@@ -1,23 +1,36 @@
 
-from matplotlib import pyplot as plt
 from makePV import makePV
 
 nPVs = 200
+nParticlesPerPV = 50
 
+
+# generate PVs
+PVs = {}
+for i in range(nPVs) :
+  PVs[i] = makePV()
+
+# generate particles
+#particles = {}
+#  for i in range(nParticlesPerPV) :
+    
+
+
+
+
+
+
+# plot PVs
 zList = []
 tList = []
-for i in range(nPVs) :
-  #newpv = makePV(ztcorr = 1.)
-  newpv = makePV()
-  zList += [newpv[3]]
-  tList += [newpv[0]]
-
-plt.scatter(zList,tList)
-plt.xlabel('PV z [mm]')
-plt.ylabel('PV t [ns]')
-plt.show()
-
-
+for key,pv in PVs.iteritems() :
+  zList += [pv[3]]
+  tList += [pv[0]]
+from matplotlib import pyplot as pltPV
+pltPV.scatter(zList,tList)
+pltPV.xlabel('PV z [mm]')
+pltPV.ylabel('PV t [ns]')
+pltPV.show()
 
 
 
