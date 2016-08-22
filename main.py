353b5719242f23e0e21b54c5ffe5b607c3493030
Laurent Dufour <laurent.dufour@cern.ch>
2016-08-22 16:58:23 -0700

    Changed dict to list

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-
 from makePV import makePV
+import detector
+import propagate
+
+nTrials = 1
 
 nPVs = 200
 nParticlesPerPV = 50
 
 
 # generate PVs
-PVs = {}
+PVs = [0] * nPVs
 for i in range(nPVs) :
   PVs[i] = makePV()
 
 # generate particles
-#particles = {}
+#particles = [] * nParticlesPerPV
 #  for i in range(nParticlesPerPV) :
     
 
@@ -23,7 +26,7 @@ for i in range(nPVs) :
 # plot PVs
 zList = []
 tList = []
-for key,pv in PVs.iteritems() :
+for pv in PVs :
   zList += [pv[3]]
   tList += [pv[0]]
 from matplotlib import pyplot as pltPV
